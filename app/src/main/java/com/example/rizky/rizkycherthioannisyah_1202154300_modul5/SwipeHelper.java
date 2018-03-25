5ca8d3045a77ce24ebb2d42cefffb04b970181ef
RizkyCherthioAnnisyah <36083140+RizkyCherthioAnnisyah@users.noreply.github.com>
2018-03-25 23:01:30 +0700

    push final

---
+++ app/src/main/java/com/example/rizky/rizkycherthioannisyah_1202154300_modul5/SwipeHelper.java
@@ -0,0 +1,47 @@
+package com.example.rizky.rizkycherthioannisyah_1202154300_modul5;
+
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+
+/**
+ * Created by RizkyCherthioAnnisyah_1202154300_MODUL5 on 3/24/2018.
+ */
+
+public class SwipeHelper extends ItemTouchHelper.SimpleCallback {
+    RecyclerViewAdapter adapter;
+
+    /**
+     * Creates a Callback for the given drag and swipe allowance. These values serve as
+     * defaults
+     * and if you want to customize behavior per ViewHolder, you can override
+     * {@link #getSwipeDirs(RecyclerView, ViewHolder)}
+     * and / or {@link #getDragDirs(RecyclerView, ViewHolder)}.
+     *
+     * @param dragDirs  Binary OR of direction flags in which the Views can be dragged. Must be
+     *                  composed of {@link #LEFT}, {@link #RIGHT}, {@link #START}, {@link
+     *                  #END},
+     *                  {@link #UP} and {@link #DOWN}.
+     * @param swipeDirs Binary OR of direction flags in which the Views can be swiped. Must be
+     *                  composed of {@link #LEFT}, {@link #RIGHT}, {@link #START}, {@link
+     *                  #END},
+     *                  {@link #UP} and {@link #DOWN}.
+     */
+    public SwipeHelper(int dragDirs, int swipeDirs) {
+        super(dragDirs, swipeDirs);
+    }
+
+    public SwipeHelper(RecyclerViewAdapter adapter){
+        super(ItemTouchHelper.UP | ItemTouchHelper.DOWN, ItemTouchHelper.LEFT);
+        this.adapter = adapter;
+    }
+
+    @Override
+    public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+        return false;
+    }
+
+    @Override
+    public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+        adapter.dismissData(viewHolder.getAdapterPosition());
+    }
+}
